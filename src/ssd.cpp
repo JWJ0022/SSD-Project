ccf391537e452e41ae923e0a47362ee4c18716c9
woojin <tngkr2010@tukorea.ac.kr>
2024-09-13 17:32:30 +0900

    .gitignore 수정

--- src/ssd.cpp
+++ src/ssd.cpp
@@ -1,2 +1,18 @@
 // ssd.cpp
+#include <iostream>
+#include <string>
 
+void init();
+void ssd_read(int index);
+void ssd_write(int index, std::string data);
+
+#define LBA_SIZE 4
+#define MAX_LBA 100
+#define INIT_DATA “0x00000000”
+
+int main(void)
+{
+
+
+    return 0;
+}
